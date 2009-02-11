56cfe68e9ecd4c2df6ec7d7237020e346e23a210
halleck1 <halleck1@a030213b-734a-0410-b26d-8bb277fb20b8>
2009-02-11 16:54:13 +0000

    Localization is finally working, part of the interface has been localized into Ukrainian

--- BzReader/Program.cs
+++ BzReader/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Globalization;
+using System.Threading;
 
 namespace BzReader
 {
@@ -12,6 +14,16 @@ namespace BzReader
         [STAThread]
         static void Main()
         {
+            CultureInfo formattingCulture = Thread.CurrentThread.CurrentCulture;
+            CultureInfo uiCulture = Thread.CurrentThread.CurrentUICulture;
+
+            // A lot of people use English Windows even though their native language is different
+
+            if (formattingCulture != uiCulture)
+            {
+                Thread.CurrentThread.CurrentUICulture = formattingCulture;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BrowseForm());
