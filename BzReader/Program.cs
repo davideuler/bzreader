a81e0b9deedc47e8dde8425cdc10d35b5c082a41
halleck1 <halleck1@a030213b-734a-0410-b26d-8bb277fb20b8>
2009-02-21 16:11:27 +0000

    Added translations, switched to fresh WiX 3, v1.0.10

--- BzReader/Program.cs
+++ BzReader/Program.cs
@@ -14,19 +14,32 @@ namespace BzReader
         [STAThread]
         static void Main()
         {
-            CultureInfo formattingCulture = Thread.CurrentThread.CurrentCulture;
-            CultureInfo uiCulture = Thread.CurrentThread.CurrentUICulture;
-
-            // A lot of people use English Windows even though their native language is different
-
-            if (formattingCulture != uiCulture)
-            {
-                Thread.CurrentThread.CurrentUICulture = formattingCulture;
-            }
+            SetUILanguage();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BrowseForm());
+        }
+
+        /// <summary>
+        /// Sets the proper culture for UI
+        /// </summary>
+        private static void SetUILanguage()
+        {
+            CultureInfo formattingCulture = Thread.CurrentThread.CurrentCulture;
+            CultureInfo uiCulture = Thread.CurrentThread.CurrentUICulture;
+
+            CultureInfo nonEnglishCulture = uiCulture;
+
+            // A lot of people use English Windows even though their native language is different
+
+            if (!formattingCulture.TwoLetterISOLanguageName.Equals("en", StringComparison.InvariantCultureIgnoreCase))
+            {
+                nonEnglishCulture = formattingCulture;
+            }
+
+            Thread.CurrentThread.CurrentCulture = nonEnglishCulture;
+            Thread.CurrentThread.CurrentUICulture = nonEnglishCulture;
         }
     }
 }
